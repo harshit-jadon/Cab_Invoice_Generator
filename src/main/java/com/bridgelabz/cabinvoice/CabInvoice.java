c9fc586099088ee628f53693d9492f2b6d6847b4
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-16 13:05:08 +0530

    Welcome Message

---
+++ src/main/java/com/bridgelabz/cabinvoice/CabInvoice.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.cabinvoice;
+
+public class CabInvoice {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CAB INVOICE GENERATOR");
+    }
+    }
---
+++ src/test/java/com/bridgelabz/testcabinvoice/CabInvoiceTest.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.testcabinvoice;
+
+public class CabInvoiceTest {
+
+}
