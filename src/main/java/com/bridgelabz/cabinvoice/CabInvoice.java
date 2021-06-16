fdff2cb7f8f868e0f00923cb75ee3dfe04ef0d0f
Harshit Jadon <harshit19jadon31@gmail.com>
2021-06-16 13:19:15 +0530

    Add-TDD Method for Total Fare of Journey

--- src/main/java/com/bridgelabz/cabinvoice/CabInvoice.java
+++ src/main/java/com/bridgelabz/cabinvoice/CabInvoice.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.cabinvoice;
 
 public class CabInvoice {
+    private static final int COST_PER_KM = 10;
+    public double totalFareOfJourney(double distance, int time) {
+        return (distance*COST_PER_KM) + time;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to CAB INVOICE GENERATOR");
     }
--- src/test/java/com/bridgelabz/testcabinvoice/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/testcabinvoice/CabInvoiceTest.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.testcabinvoice;
 
-public class CabInvoiceTest {
+import com.bridgelabz.cabinvoice.CabInvoice;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class CabInvoiceTest {
+    @Test
+    public void givenDistanceAndTime_whenCostPerKilometrePerMin_shouldReturnTotalFare() {
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.totalFareOfJourney(7,15);
+        Assert.assertEquals(85,totalFare, 0.0);
+    }
 }
